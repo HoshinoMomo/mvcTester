678ed3d803c1d377345f05ac3460de5d49f512da
easyzhang <692051437@qq.com>
2018-12-05 23:16:16 +0800

    base

---
+++ src/main/java/com.github.easyzhang/manyusertest/ManyUserTest.java
@@ -0,0 +1,12 @@
+package com.github.easyzhang.manyusertest;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+
+public class ManyUserTest {
+    public static void main(String[] args) throws IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+    }
+}
